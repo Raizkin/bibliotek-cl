a359502ba7b97ab1c3dad5b940f6091e84914b51
Raizkin <148555404+Raizkin@users.noreply.github.com>
2025-11-04 18:02:07 +0200

    fixall

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import Chart from "chart.js/auto";
+import { alert, defaultModules, success, error, info } from "@pnotify/core/dist/PNotify.js";
+import * as PNotifyMobile from "@pnotify/mobile/dist/PNotifyMobile.js";
+import "@pnotify/core/dist/BrightTheme.css";
+import "@pnotify/mobile/dist/PNotifyMobile.css";
+import "@pnotify/core/dist/PNotify.css";
+
+defaultModules.set(PNotifyMobile, {});
 
 const chartData = {
-  labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+  labels: Array.from({ length: 30 }, (_, i) => (i + 1).toString()),
   datasets: [
     {
       label: "Продажі за останній місяць",
@@ -12,51 +19,44 @@ const chartData = {
     },
   ],
 };
+
 const config = {
-  type: 'line',
+  type: "line",
   data: chartData,
 };
-const canvasRef = document.getElementById("sales-chart")
-const salesChart = new Chart(canvasRef,config);
 
-import { alert, defaultModules,success,error,info } from "@pnotify/core/dist/PNotify.js";
-
-import * as PNotifyMobile from "@pnotify/mobile/dist/PNotifyMobile.js";
-import "@pnotify/core/dist/BrightTheme.css";
-import "@pnotify/mobile/dist/PNotifyMobile.css";
-import "@pnotify/core/dist/PNotify.css";
-
-defaultModules.set(PNotifyMobile, {});
+const canvasRef = document.getElementById("sales-chart");
+const salesChart = new Chart(canvasRef, config);
 
 const boxEl = document.querySelector(".box");
-
 const keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+let currentKeyIndex = 0;
 
-let currentKeyIndex = 0
 boxEl.textContent = keys[currentKeyIndex];
-window.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-boxEl.textContent = "Bye"
-info({
-  title: 'Допобачення',
-  text: 'Ви закінчили гру.'
-});
 
-return
-    }
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    boxEl.textContent = "Bye";
+    info({
+      title: "Допобачення",
+      text: "Ви закінчили гру.",
+    });
+    return;
+  }
+
   if (keys[currentKeyIndex] === event.key) {
-    currentKeyIndex = Math.floor(Math.random() * (keys.length - 1) + 1);
+    currentKeyIndex = Math.floor(Math.random() * keys.length);
     boxEl.textContent = keys[currentKeyIndex];
     success({
       title: "Ураа!",
       text: "Ви натиснули правильну літеру.",
-      delay: 500
+      delay: 500,
     });
   } else {
- error({
-  title: 'О ні',
-  text: 'Ви натиснули неправильну літеру.',
-  delay: 500
-});
+    error({
+      title: "О ні",
+      text: "Ви натиснули неправильну літеру.",
+      delay: 500,
+    });
   }
 });
